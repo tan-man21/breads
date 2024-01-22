94f46beb72f27a13eac1be31ee9ad52844d47b4f
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-01-21 21:47:18 -0500

    Added list of breads on index page

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,6 +2,13 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread')
 
+breads.get('/', (req, res) => {
+    res.render('index', {
+        breads: Bread,
+        title: 'Index Page'
+    })
+})
+
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])
 })
