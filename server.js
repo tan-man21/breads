abf23ea1e0799d694c73f4aa8b9260303bf50bf2
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-01-23 18:44:30 -0500

    Added basic css styling and show page for bread types

--- server.js
+++ server.js
@@ -8,10 +8,16 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
 
     res.send('Welcome to an Awesome app about Breads')
 });
+
+app.get('*', (req, res) => {
+    res.send('404')
+})
 
 const breadsControllers = require('./controllers/breads_controller')
 app.use('/breads', breadsControllers)
