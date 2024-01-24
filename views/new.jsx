6b2f9445fdbe379b1a2da90a59408dcd415bca11
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-01-23 20:07:34 -0500

    required correct image submission

--- views/new.jsx
+++ views/new.jsx
@@ -18,7 +18,8 @@ function New() {
                 <label htmlFor="image">Image</label>
                 <input type="text"
                 name='image'
-                id='image' 
+                id='image'
+                pattern="https?://.+" title="Include http://" 
                 />
                 <label htmlFor="hasGluten">Has Gluten?</label>
                 <input type="checkbox"
