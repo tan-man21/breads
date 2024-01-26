adfd28976959980cc44e8574901bc632e4065907
Tanner Rogers <trroger2@alumni.ncsu.edu>
2024-01-25 19:32:46 -0500

    edited button layout on show page

--- views/show.jsx
+++ views/show.jsx
@@ -16,11 +16,19 @@ function Show ({bread, index}) {
                 have gluten.
             </p>
             <img src={bread.image} alt={bread.name}/>
-            <li><a href="/breads"><button>Go Home</button></a></li>
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+            <span>
+            <a href="/breads"><button>Go Home</button></a>
+            </span>
+            <span>
             <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+            </span>
+            <span>
             <form action={`/breads/${index}?_method=DELETE`} method='POST'>
                 <input type="submit" value="DELETE" />
             </form>
+            </span>
+            </div>
         </Default>
     )
 }
